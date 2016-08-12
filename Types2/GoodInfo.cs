04e8e312ad4afab28a28197a7e0fbf1b1f61d6bb
VolhaShyrko <olgashyrko@gmail.com>
2016-08-12 11:05:55 +0300

    first commit

---
+++ Types2/GoodInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace Types2
+{
+    using System.Runtime.InteropServices;
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct GoodInfo
+    {
+        public int Quantity;
+        public decimal Price;
+        public string Good;
+
+        public GoodInfo(string good, decimal price, int quantity)
+        {
+            this.Good = good;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+    }
+}
---
+++ Types2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Types2
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Store!");
+            Console.WriteLine("Here is the goods list we can offer:\r\n");
+
+            List<GoodInfo> list = GetGoodslist();
+            foreach (GoodInfo item in list)
+            {
+                Console.WriteLine(string.Format("Item = {0} : Quantity = {1} : Price = {2} ", item.Good, item.Quantity, item.Price));
+            }
+
+            
+            Console.ReadKey();
+
+        }
+
+        private static List<GoodInfo> GetGoodslist()
+        {
+            List<GoodInfo> list = new List<GoodInfo>
+                                      {
+                                          new GoodInfo("Book", 12.49M, 1),
+                                          new GoodInfo("Music CD", 14.99M, 1),
+                                          new GoodInfo("Chocolate Bar", 8.5M, 1)
+                                      };
+
+            return list;
+        }
+    }
+}
